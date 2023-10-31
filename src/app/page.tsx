3da28a1c2976ff4a1468974b520d3dc8c39730c0
Daugardas <luksasdaugardas@gmail.com>
2023-10-31 22:32:56 +0200

    Refactor page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,3 @@
-import React from "react";
-
-export default function Home() {
-
-  return (
-    <>
-      <div className="">
-        pagrindinis puslapis /
-      </div>
-    </>
-  );
+export default function Page() {
+  return <div className="">pagrindinis puslapis /</div>;
 }
