c7452d69bb2fe1e2b7d4250bdf740452bb6f5548
Daugardas <luksasdaugardas@gmail.com>
2023-10-31 15:55:42 +0200

    Update NextAuth configuration to include CredentialsProvider

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,69 +1,29 @@
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthConfig } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { Naudotojas } from "@prisma/client";
 import prisma from "@/lib/db";
 import { compare } from "bcrypt";
 
+const authConfig: NextAuthConfig = {
+  providers: [
+    CredentialsProvider({
+      name: "signIn",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "example@example.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      authorize(credentials) {
+        return null;
+      },
+    }),
+  ],
+};
+
 export const {
-  handlers: { GET, POST},
+  handlers: { GET, POST },
   auth,
-} = NextAuth({providers: [CredentialsProvider]})
-
-// export const authOptions: NextAuthOptions = {
-//   session: {
-//     strategy: "jwt",
-//   },
-//   adapter: PrismaAdapter(prisma),
-//   pages: {
-//     signIn: "/login",
-//     newUser: "/register",
-//   },
-//   providers: [
-//     CredentialsProvider({
-//       name: "signIn",
-//       credentials: {
-//         email: {
-//           label: "Email",
-//           type: "email",
-//           placeholder: "example@example.com",
-//         },
-//         password: { label: "Password", type: "password" },
-//       },
-//       async authorize(credentials) {
-//         // if (!credentials || !credentials.email || !credentials.password) {
-//         //   return null;
-//         // }
-
-//         // const naudotojas: Naudotojas | null =
-//         //   await prisma.naudotojas.findUnique({
-//         //     where: { el_pastas: credentials.email },
-//         //   });
-
-//         // if (
-//         //   naudotojas &&
-//         //   (await compare(credentials.password, naudotojas.slaptazodis))
-//         // ) {
-//         //   // return user without any sensitive information
-//         //   return {
-//         //     id: naudotojas.id,
-//         //     email: naudotojas.el_pastas,
-//         //   };
-//         // }
-
-//         return { id: "156ds", email: "test@test"};
-//       },
-//     }),
-//   ],
-//   callbacks: {
-//     session: async ({ session, token }) => {
-//       return { ...session, user: { id: token.id, email: token.email } };
-//     },
-//     jwt: async ({ token, user }) => {
-//       if (user) {
-//         token.id = user.id;
-//         token.email = user.email;
-//       }
-//       return token;
-//     },
-//   },
-// };
+} = NextAuth(authConfig);
