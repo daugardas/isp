6478d230939fcb0fdb2780dfbb3b335cb8e50e05
Mindaugas Padegimas <minpad1@ktu.lt>
2023-11-04 20:25:40 +0200

    add basic default pages for pranesimai

--- src/app/pranesimai/page.tsx
+++ src/app/pranesimai/page.tsx
@@ -1,8 +1,14 @@
 import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
+import Link from "next/link";
 
 export default async function Page() {
   const session = await auth();
   if (!session) redirect("/auth/signin");
-  return <div>pranesimu psl</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Link href="/pranesimai/send">Siusti</Link>
+      {/* <Link href="/pranesimai/module" className="mt-4 underline text-blue-600">Matematika1</Link> */}
+    </div>
+  );
 }
