26e505e9ef01920e419cb8ad815de9e1472a08e7
Mindaugas Padegimas <minpad1@ktu.lt>
2023-11-05 00:54:55 +0200

    add interfaces for different pages of pranesimai

--- src/app/pranesimai/delete/page.tsx
+++ src/app/pranesimai/delete/page.tsx
@@ -1,8 +1,50 @@
+'use client'
 import { auth } from "@/lib/auth";
-import { redirect } from "next/navigation";
+import { useRouter } from 'next/navigation';
+import Link from "next/link";
+import React, { useState } from "react";
 
-export default async function Page() {
-  const session = await auth();
-  if (!session) redirect("/auth/signin");
-  return <div>Pranesimu trinimo psl</div>;
+export default  function Page() {
+  const router = useRouter();
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    router.push('/pranesimai');
+  };
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
+  const handleDelete = () => {
+    console.log("Delete button clicked");
+  };
+
+  const message = "Žinutė, kuria norime ištrinti";
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="message-container">
+          <div 
+            className="text-lg text-gray-700">{message}
+          </div>
+          <label className="checkbox-label">
+            <input type="checkbox" checked={isChecked} onChange={handleCheckboxChange} />
+          </label>
+      </div>
+      {isChecked && (
+      <div className="flex items-left justify-left">
+          <button
+            onClick={handleDelete}
+            type="submit"
+            className="bg-blue-400 hover:bg-red-800 transition duration-1000 ease text-white py-2 px-4 rounded-md">
+            Ištrinti pasirinktas žinutes
+          </button>
+      </div>
+      )}
+      
+      <div className="flex flex-col items-left justify-center h-screen text-gray-500">
+          <Link href="/pranesimai" className="hover:text-black transition duration-300">Grįžti atgal į pranešimus</Link>
+      </div>
+    </div>
+  );
 }
--- src/app/pranesimai/page.tsx
+++ src/app/pranesimai/page.tsx
@@ -2,13 +2,17 @@ import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
 import Link from "next/link";
 
+
 export default async function Page() {
   const session = await auth();
   if (!session) redirect("/auth/signin");
+  
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <Link href="/pranesimai/send">Siusti</Link>
-      {/* <Link href="/pranesimai/module" className="mt-4 underline text-blue-600">Matematika1</Link> */}
-    </div>
+      <div className="flex flex-col items-start justify-center h-screen text-gray-500">
+        <Link href="/pranesimai/send" className="hover:text-black transition duration-300">Siūsti pranešimą</Link>
+        <Link href="/pranesimai/delete" className="hover:text-black transition duration-300">Trinti pranešimą</Link>
+        <Link href="/pranesimai/settings" className="hover:text-black transition duration-300">Pranešimų nustatymai</Link>
+        {/* <Link href="/pranesimai/module" className="mt-4 underline text-blue-600">Matematika1</Link> */}
+      </div>
   );
 }
