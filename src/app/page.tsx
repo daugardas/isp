113b5aad0c7ad779fa332012ffd10145d0650654
Aivaras Armalis <aivarm@ktu.lt>
2023-12-11 22:10:47 +0200

    feat: add content to the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
 export default function Page() {
-  return <div className="">pagrindinis puslapis /</div>;
+    const gifUrl = "https://i.pinimg.com/originals/57/61/5b/57615b8c0092a66c1d4058b1692955cc.gif"; // Replace with your actual GIF URL
+
+    return (
+        <div className="">
+            Pagrindinis puslapis 
+            <img src={gifUrl} alt="Description of GIF" /> {/* Add this line */}
+        </div>
+    );
 }
