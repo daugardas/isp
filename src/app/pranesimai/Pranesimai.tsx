e99e2d6be9024390b29e5bc04ce6e166e55118d9
Mindaugas Padegimas <minpad1@ktu.lt>
2023-12-17 01:09:09 +0200

    paprastas

--- src/app/pranesimai/Pranesimai.tsx
+++ src/app/pranesimai/Pranesimai.tsx
@@ -2,16 +2,27 @@
 import React from "react";
 import Link from "next/link";
 import { MinimizedPranesimai } from "../api/pranesimai/search/route";
+import { Session } from "inspector";
 
 type PranesimaiProps = {
   messages: MinimizedPranesimai[]; // Adjust the type based on your data model
+
+};
+const getUserRole = () => {
+  return
 };
 
 export default function Pranesimai({ messages }: PranesimaiProps) {
+
   return (
     <div>
       <div className="bg-blue-500 text-white p-4 mt-auto flex justify-between space-x-4">
-        <Link href="/pranesimai/send" className="text-1xl font-semibold hover:text-red-700">Siūsti pranešimą</Link>
+
+        {messages.map(message => message.naudotojas.tipas === 'administratorius') && (
+          <Link href="/pranesimai/send" className="text-1xl font-semibold hover:text-red-700">
+            Siūsti pranešimą
+          </Link>
+        )}
         <Link href="/pranesimai/delete" className="text-1xl font-semibold hover:text-red-700">Trinti pranešimą</Link>
         <Link href="/pranesimai/settings" className="text-1xl font-semibold hover:text-red-700">Pranešimų nustatymai</Link>
       </div>
